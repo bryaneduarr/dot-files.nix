ba78f27df25faf0f0f708001e275b7022cebe9c2
bryaneduarr <bryaneduarr@gmail.com>
2025-08-29 18:49:10 +0000

    added powerlevel10k to the configuration
    It seems that the only thing to do is to use it.
    
    I included the new '.p10k.zsh' in here, I think the configuration
    might be useful.

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -1,8 +1,17 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   programs.zsh = {
     enable = true; # Enable ZSH here too, it is also enabled in the 'configuration.nix' file.
+
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "jeffreytse/zsh-vi-mode"; }
+        { name = "romkatv/powerlevel10k"; tags = [ "as:theme" "depth:1" ]; }
+      ];
+    };
   
     # Inline shell customizations (functions, exports, prompts, etc.).
     initContent = ''
@@ -12,7 +21,21 @@
       }
   
       # Set the default editor to Neovim.
-    export EDITOR=nvim
+      export EDITOR=nvim
+
+      # Source the Powerlevel10k theme and configuration.
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      source ${config.home.homeDirectory}/.p10k.zsh
     '';
+  };
+
+  # Make sure the p10k package is available.
+  home.packages = [
+    pkgs.zsh-powerlevel10k
+  ];
+
+  # Tell where the configuration file for p10k is.
+  home.file = {
+    ".p10k.zsh".source = ./../.p10k.zsh;
   };
 }
