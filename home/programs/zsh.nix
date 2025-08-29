c903ac6f80af03cc02ff456837298ca0e46ff29c
bryaneduarr <bryaneduarr@gmail.com>
2025-08-29 19:27:56 +0000

    added autocompletion and history to zsh
    Added new plugins that interact with it.

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -4,12 +4,53 @@
   programs.zsh = {
     enable = true; # Enable ZSH here too, it is also enabled in the 'configuration.nix' file.
 
+    history = {
+      size = 10000; # Limit of history.
+      save = 10000; # Limit of history.
+      path = "${config.home.homeDirectory}/.zsh_history"; # The directory will be created automatically by home-manager.
+
+     # setopt HIST_FCNTL_LOCK.
+      fcntlLocks = true;
+      
+      # setopt APPEND_HISTORY.
+      append = true;
+
+      # setopt HIST_IGNORE_DUPS.
+      ignoreDups = true;
+      
+      # setopt HIST_IGNORE_ALL_DUPS.
+      ignoreAllDups = true;
+
+      # setopt HIST_SAVE_NO_DUPS.
+      saveNoDups = true;
+
+      # setopt HIST_FIND_NO_DUPS.
+      findNoDups = true;
+
+      # setopt HIST_IGNORE_SPACE.
+      ignoreSpace = true;
+
+      # setopt HIST_EXPIRE_DUPS_FIRST.
+      expireDuplicatesFirst = true;
+
+      # setopt SHARE_HISTORY.
+      share = true;
+
+      # unsetopt EXTENDED_HISTORY
+      extended = false;
+    }
+
+    # All of the plugins will be managed by 'zplug'.
     zplug = {
-      enable = true;
+      enable = true; # Enable zplug.
+
+      # Place all of the plugins for ZSH in here.
       plugins = [
         { name = "zsh-users/zsh-syntax-highlighting"; }
         { name = "jeffreytse/zsh-vi-mode"; }
         { name = "romkatv/powerlevel10k"; tags = [ "as:theme" "depth:1" ]; }
+	{ name = "zsh-users/zsh-completions"; }
+	{ name = "zsh-users/zsh-autosuggestions"; }
       ];
     };
   
