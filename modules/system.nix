c1c578e07f4041ccfbafc65d9338de0f9732263b
bryaneduarr <bryaneduarr@gmail.com>
2025-09-04 21:27:15 +0000

    added postgresql in the system packages

--- modules/system.nix
+++ modules/system.nix
@@ -5,11 +5,22 @@
 {
   # Enable better Nix commands for interaction. Enable NixOS experimental flakes.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
+
   programs.zsh.enable = true; # Enable in the system to have ZSH as a shell.
 
   security.sudo.enable = true; # Enable sudo command in the system.
   security.sudo.wheelNeedsPassword = true; # Every wheel user 'with sudo privileges' will be prompted to use the password.
+
+  # Install PostgreSQL CLI tools system-wide.
+  environment.systemPackages = with pkgs; [
+    postgresql
+  ];
+
+  # Enable and configure PostgreSQL service.
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql;
+  };
 
   # NixOS version configuration state always to the latest.
   system.stateVersion = "25.05";
