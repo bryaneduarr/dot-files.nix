d228d7609c36c324a2af6df5ecf46313b983c5ec
Bryan Eduardo <bryaneduarr@nixos.localdomain>
2025-11-07 13:52:52 +0000

    modified auto-cmds to include new functionalities
    This funtionalities come from:
    https://gist.github.com/smnatale/692ac4f256d5f19fbcbb78fe32c87604
    
    Are not personal, but are some real cool ways for improvement.
    
    Changed yazi las session keymap.

--- nvim/lua/config/auto-cmds.lua
+++ nvim/lua/config/auto-cmds.lua
@@ -10,3 +10,62 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+-- Got this auto commands from the web: https://gist.github.com/smnatale/692ac4f256d5f19fbcbb78fe32c87604
+-- No auto continue comments on new line.
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
+-- Syntax highlighting for dotenv files.
+vim.api.nvim_create_autocmd("BufRead", {
+  group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+  pattern = { ".env", ".env.*" },
+  callback = function()
+    vim.bo.filetype = "dosini"
+  end,
+})
+
+-- Show cursorline only in active window enable.
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+-- Show cursorline only in active window disable.
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  group = "active_cursorline",
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
+
+-- IDE like highlight when stopping cursor.
+vim.api.nvim_create_autocmd("CursorMoved", {
+  group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
+  desc = "Highlight references under cursor",
+  callback = function()
+    -- Only run if the cursor is not in insert mode
+    if vim.fn.mode() ~= "i" then
+      local clients = vim.lsp.get_clients({ bufnr = 0 })
+      local supports_highlight = false
+      for _, client in ipairs(clients) do
+        if client.server_capabilities.documentHighlightProvider then
+          supports_highlight = true
+          break -- Found a supporting client, no need to check others
+        end
+      end
+
+      -- 3. Proceed only if an LSP is active AND supports the feature
+      if supports_highlight then
+        vim.lsp.buf.clear_references()
+        vim.lsp.buf.document_highlight()
+      end
+    end
+  end,
+})
