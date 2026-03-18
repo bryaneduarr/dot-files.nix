fba8bd7a03b87e86a3ae48780a9c88b4b7033fcf
Bryan Eduardo <bryaneduarr@nixos.localdomain>
2026-03-18 15:48:02 -0600

    added nix, vim and clangd support for the LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,13 +4,3 @@ require("config.keymaps")
 require("config.statusline")
 -- require("config.winbar")
 require("config.auto-cmds")
-
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
-})
